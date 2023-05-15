f4a375a300ef365a4c660cb81049c64e12a3bb48
Jhonnar Rodriguez <jhonnar.rodriguez@gmail.com>
2023-05-15 07:42:22 -0500

    feat(backend): swagger enabled just for development environments

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  if (['development', 'development', 'local'].includes(process.env.NODE_ENV)) {
+    const options = new DocumentBuilder()
+      .setTitle('Git History')
+      .setDescription(
+        'Provide the required endpoints for our app to show git commit history on the same project we are working on.',
+      )
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup('api/swagger', app, document);
+  }
+
   await app.listen(3000);
+
+  // eslint-disable-next-line no-console
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
